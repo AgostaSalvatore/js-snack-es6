6603c4e5923c294566efb1539dc3aed0996c7549
AgostaSalvatore <agosta.boolean@gmail.com>
2025-03-28 14:08:49 +0100

    Add array of bike objects to script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
+//Snack 1 -- Esercizio 1
+//creare un array di oggetti bici
+const bikes = [
+    {
+        name: "Bici-1",
+        weight: 10
+    },
+    {
+        name: "Bici-2",
+        weight: 20
+    },
+    {
+        name: "Bici-3",
+        weight: 30
+    }
+]
 
